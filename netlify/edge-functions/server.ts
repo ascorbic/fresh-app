70898c0155b91dc29d03d3616072cd2f0798a44f
ascorbic <ascorbic@gmail.com>
2022-11-13 10:20:42 +0000

    Initial commit

---
+++ netlify/edge-functions/server.ts
@@ -0,0 +1,39 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { Context } from "https://edge.netlify.com";
+import { ServerContext } from "$fresh/server.ts";
+import manifest from "../../fresh.gen.ts";
+
+const ctx = await ServerContext.fromManifest(manifest, {
+	staticDir: "_invalid",
+});
+
+const staticManifest = new Set(["/favicon.ico", "/logo.svg"]);
+
+const freshHandler = ctx.handler();
+
+export default async function handler(request: Request, context: Context) {
+	console.log(Deno.run);
+	const requestURL = new URL(request.url);
+	const pathname = requestURL.pathname;
+	console.log("pathname", pathname);
+	if (staticManifest.has(pathname)) {
+		return;
+	}
+	return await freshHandler(request, {
+		localAddr: {
+			hostname: requestURL.hostname,
+			port: Number(requestURL.port),
+			transport: "tcp",
+		},
+		remoteAddr: {
+			hostname: context.ip,
+			port: 0,
+			transport: "tcp",
+		},
+	});
+}
